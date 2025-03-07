4bb7bcc7d133a6932a89fe7db9f91dc167fc9aba
Inshalmunaf <inshalmunaf@gmail.com>
2025-03-07 23:56:41 +0500

    final touches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <div className="w-full ">
-                <div className="max-w-screen-xl mx-auto flex gap-30">
+            <div className="pb-30 w-full ">
+                <div className="max-w-screen-xl mx-auto flex gap-30 ">
                     <div className="basis-1/2">
                         <h1 className='text-[11rem] font-semibold leading-none tracking-tight'>refokus.</h1>
                     </div>
@@ -17,7 +17,7 @@ function Footer() {
                             <h1 className='mb-10 capitalize text-zinc-500'>socials</h1>
                             {["Home", "About", "Contact", "more"].map((item, index) => <a  key ={index} className='block mt-3 capitalize text-zinc-500'>{item}</a>)}
                         </div>
-                        <div className="basis-1/2 flex flex-col items-end">
+                        <div className="pt-10 basis-1/2 flex flex-col items-end">
                             <p className='text-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis iusto error ea fugiat tenetur ex!</p>
                             <span className=' mt-3 text-right w-2/3 px-3 py-2 bg-violet-600'>Empower</span>
                         </div>
